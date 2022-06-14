0fb199df991d6d751adfdae99be64b8eb41ec84f
Samantha Stockall <sam.stockall@hotmail.com>
2022-06-14 14:03:57 -0400

    Added .get for warehouse list

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const warehoueseFile = fs.readFileSync('./data/warehouses.json') 
+
+router.route('/')
+    .get((_req, res) => {
+        const warehoueses = JSON.parse(warehoueseFile);
+        res.json(warehoueses)
+    })
 
 module.exports = router;
