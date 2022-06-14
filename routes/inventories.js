1e9d7abea372de2728cfa577722913cd01e8f68c
Suraya <surayaclemens@gmail.com>
2022-06-14 11:56:19 -0700

    inventories backend get ready to push

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const fs = require('fs');
 const inventoryFile = fs.readFileSync('../data/inventories.json');
 
-const inventoryParsed = JSON.parse(inventoryFile);
-
 // GET LIST OF ALL INVENTORY ITEMS FROM ALL WAREHOUSES
+
+router.route('/')
+    .get((_req, res) => {
+        const inventories = JSON.parse(inventoryFile);
+        res.json(inventories)
+    })
+
+module.exports = router; 
