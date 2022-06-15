8e66241264f4c02b2008371935c0a97507af442a
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-14 21:00:22 -0500

    feat: post route sending correct response. Pending validation and error handling

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,21 +3,41 @@ const router = express.Router();
 const fs = require('fs');
 var uniqid = require('uniqid')
 
-const getWareHouses = () => {
-    const warehousesFile = fs.readFileSync('./data/warehouses.json')
-    const warehouses = JSON.parse(warehousesFile)
-    
-}
 
 // T25: new inventory item
 
+// const formErrors = {
+//     name: 'Item name is required',
+//     description: 'Description is required',
+//     category: 'Category must be selected',
+//     status: 'Status must be selected',
+//     quantity: 'Quantity is required',
+//     warehouse: 'Warehouse must be selcted'
+// }
+
+// const inputValidation = (input) => {
+//     if (!input) {
+//         return 
+//     }
+// }
+const warehousesFile = fs.readFileSync('./data/warehouses.json')
+const warehouses = JSON.parse(warehousesFile)
 router.route('/new')
-.post((req, res) => {
-
-})
-
-// input: name, description, category, statys, quantity, warehouseID
-// get warehouseName, assign id
+    .post((req, res) => {
+        const { name, description, category, status, quantity, warehouseId } = req.body
+        const warehouseFound = warehouses.find(item => item.id === warehouseId)
+        let newInventoryItem = {
+            id: uniqid(),
+            warehouseID: warehouseId,
+            warehouseName: warehouseFound.name,
+            itemName: name,
+            description: description,
+            category: category,
+            status: status,
+            quantity: quantity
+        }
+        res.status(201).send(newInventoryItem)
+    })
 
 const inventoryFile = fs.readFileSync('./data/inventories.json');
 
