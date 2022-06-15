824a04db22615791d145dac7c511419c45181baa
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-14 21:28:52 -0500

    feat: saving to files and validation implemented

--- routes/inventories.js
+++ routes/inventories.js
@@ -3,46 +3,49 @@ const router = express.Router();
 const fs = require('fs');
 var uniqid = require('uniqid')
 
-
-// T25: new inventory item
-
-// const formErrors = {
-//     name: 'Item name is required',
-//     description: 'Description is required',
-//     category: 'Category must be selected',
-//     status: 'Status must be selected',
-//     quantity: 'Quantity is required',
-//     warehouse: 'Warehouse must be selcted'
-// }
-
-// const inputValidation = (input) => {
-//     if (!input) {
-//         return 
-//     }
-// }
+// ===== Add new inventory item =====
 const warehousesFile = fs.readFileSync('./data/warehouses.json')
 const warehouses = JSON.parse(warehousesFile)
 router.route('/new')
     .post((req, res) => {
         const { name, description, category, status, quantity, warehouseId } = req.body
-        const warehouseFound = warehouses.find(item => item.id === warehouseId)
-        let newInventoryItem = {
-            id: uniqid(),
-            warehouseID: warehouseId,
-            warehouseName: warehouseFound.name,
-            itemName: name,
-            description: description,
-            category: category,
-            status: status,
-            quantity: quantity
+        if (!dataIsValid(name, description, category, status, quantity, warehouseId)) {
+            res.status(400).send("Incorrect or missing information sent to server")
+        } else {
+            const warehouseFound = warehouses.find(item => item.id === warehouseId)
+            let newInventoryItem = {
+                id: uniqid(),
+                warehouseID: warehouseId,
+                warehouseName: warehouseFound.name,
+                itemName: name,
+                description: description,
+                category: category,
+                status: status,
+                quantity: quantity
+            }
+            res.status(201).send(newInventoryItem)
+            const inventories = JSON.parse(inventoryFile)
+            let updatedInventories = [...inventories, newInventoryItem]
+            fs.writeFileSync('./data/inventories.json', JSON.stringify(updatedInventories))
         }
-        res.status(201).send(newInventoryItem)
     })
+// ------ validation methods ------
+    const inputIsValid = (input) => {
+        if (!input) {
+            return false
+        }
+        return true
+    }
+    const dataIsValid = (name, description, category, status, quantity, warehouseId) => {
+        if (!inputIsValid(name) || !inputIsValid(description) || !inputIsValid(category) || !inputIsValid(status) || !inputIsValid(quantity) || !inputIsValid(warehouseId)) {
+            return false
+        } else {
+            return true
+        }
+    }
 
+// ===== Get list of all inventory items =====
 const inventoryFile = fs.readFileSync('./data/inventories.json');
-
-// GET LIST OF ALL INVENTORY ITEMS FROM ALL WAREHOUSES
-
 router.route('/')
     .get((_req, res) => {
         const inventories = JSON.parse(inventoryFile);
