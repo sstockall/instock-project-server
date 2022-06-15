aee987bc8bed01bad842df01e10a95095db8e440
Samantha Stockall <sam.stockall@hotmail.com>
2022-06-15 16:44:53 -0400

    built endpoint for getting inventory of single warehouse by id

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -2,11 +2,30 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const warehouseFile = fs.readFileSync('./data/warehouses.json') 
+const inventoriesFile = fs.readFileSync('./data/inventories.json')
 
+//Get all warehouses
 router.route('/')
     .get((_req, res) => {
         const warehouses = JSON.parse(warehouseFile);
         res.json(warehouses)
+    })
+
+//Get single warehouse ID and then use ID to get all inventories for this
+//warehouse     
+router.route('/:warehouseId')
+    .get((req, res) => {
+        const warehouses = JSON.parse(warehouseFile);
+        const inventories = JSON.parse(inventoriesFile);
+        const singleId = warehouses.find((house) => house.id === req.params.warehouseId);
+        const singleWarehouse = inventories.filter((inventory) => inventory.warehouseID === singleId.id)
+
+        if(!singleWarehouse) {
+            res.status(404).send('Warehouse not found');
+            return;
+        }
+
+        res.json(singleWarehouse);
     })
 
 module.exports = router;
