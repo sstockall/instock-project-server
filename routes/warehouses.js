0b69c0b14ee693a03f79db1a09b100ca4436f3ca
Carlos Ocampo <charlie@carlosocampo.ca>
2022-06-16 17:55:29 -0500

    return error messages implemented for new warehouse post request. ticket completed.

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -10,7 +10,8 @@ router.route('/new')
 
         const { name, address, city, country, contactName, position, phone, email } = req.body
         if (!dataIsValid(name, address, city, country, contactName, position, phone, email)) {
-            res.status(400).send("Incorrect or missing information sent to server")
+            res.status(400).send(errorMessage)
+            errorMessage = ''
         } else {
             let newWarehouse = {
                 id: uniqid(),
@@ -25,14 +26,17 @@ router.route('/new')
                     email: email
                 }
             }
-            res.status(201).send(newWarehouse)
+            res.status(201).send(`New warehouse created with id: ${newWarehouse.id}`)
             const warehouses = JSON.parse(warehouseFile)
             let updatedWarehouses = [...warehouses, newWarehouse]
-            console.log(newWarehouse)
-            // fs.writeFileSync('./data/warehouses.json', JSON.stringify(updatedWarehouses))
+            errorMessage = ''
+            fs.writeFileSync('./data/warehouses.json', JSON.stringify(updatedWarehouses))
         }
     })
 // ------ validation methods ------
+// errorMessage is returned to the post request detailing what information was invalid (missin info, wrong phone format and/or wrong email format)
+let errorMessage = ''
+
 const inputIsValid = (input) => {
     if (!input) {
         return false
@@ -40,33 +44,37 @@ const inputIsValid = (input) => {
     return true
 }
 const dataIsValid = (name, address, city, country, contactName, position, phone, email) => {
-    if (!inputIsValid(name) || !inputIsValid(address) || !inputIsValid(city) || !inputIsValid(country) || !inputIsValid(contactName) || !inputIsValid(position) || !phoneIsValid(phone) || !emailIsValid(email)) {
-        return false
-    } else {
-        return true
+    let areInputsValid = true
+    if (!inputIsValid(name) || !inputIsValid(address) || !inputIsValid(city) || !inputIsValid(country) || !inputIsValid(contactName) || !inputIsValid(position)) {
+        errorMessage = 'Incorrect or missing information.'
+        areInputsValid = false
     }
+
+    if (!phoneIsValid(phone)) {
+        errorMessage += ' Invalid phone number, please use the format "+1 (647) 123-1234".'
+        areInputsValid = false
+    }
+    if (!emailIsValid(email)) {
+        errorMessage += ' Invalid email address.'
+        areInputsValid = false
+    }
+    return areInputsValid
 }
 
 const emailIsValid = (input) => {
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (input.match(mailformat)) {
-        console.log("Valid email address!")
         return true;
-    }
-    else {
-        console.log("You have entered an invalid email address!");
+    } else {
         return false;
     }
 }
 
 const phoneIsValid = (input) => {
     let phoneformat = /^\+[0-9]+\s\(\d\d\d\)\s\d\d\d-\d\d\d\d$/i;
-    if(input.match(phoneformat)) {
-        console.log("Valid phone number!")
+    if (input.match(phoneformat)) {
         return true;
-    }
-    else {
-        console.log("You have entered an invalid phone number!");
+    } else {
         return false;
     }
 }
